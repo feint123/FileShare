01b7b3d32b04d8442ef57b193e6f2e22d7c258cf
feint123 <1084645640@qq.com>
2017-07-15 16:29:04 +0800

    share
    share

--- linkedstack.h
+++ linkedstack.h
@@ -1,5 +1,8 @@
-//#ifndef _LINKEDSTACK_H
-//#define _LINKEDSTACK_H
+#ifndef _LINKEDSTACK_H
+#define _LINKEDSTACK_H
+
+//if you want to use stack with a header ,please remove this define
+#define _NO_HEADER
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -14,12 +17,42 @@ typedef struct StackNode{
 }snode, *linkedstack;
 
 
+#ifdef _NO_HEADER
+void initStack(linkedstack *stack)
+{
+	(*stack) = NULL;
+}
+int push(linkedstack* stack, ElemType e)
+{
+	snode *node = (snode*)malloc(sizeof(snode));
+	node->data = e;
+	node->next =(*stack);
+	(*stack) = node;
+	return true;
+}
+ElemType pop(linkedstack* stack){
+	snode *node = (*stack);
+	if (node == NULL)
+		return NULL;
+	(*stack) = node->next;
+	ElemType e = node->data;
+	free(node);
+	return e;
+}
+int emptyStack(linkedstack stack)
+{
+	if (stack == NULL){
+		return true;
+	}
+	else
+		return false;
+}
+#else
 void initStack(linkedstack *stack)
 {
 	(*stack) = (linkedstack)malloc(sizeof(snode));
 	(*stack)->next = NULL;
 }
-
 int emptyStack(linkedstack stack)
 {
 	if (stack->next == NULL){
@@ -29,8 +62,9 @@ int emptyStack(linkedstack stack)
 		return false;
 }
 
-int push(linkedstack stack,ElemType e)
+int push(linkedstack stack, ElemType e)
 {
+
 	snode *node = (snode*)malloc(sizeof(snode));
 	node->data = e;
 	node->next = stack->next;
@@ -48,4 +82,7 @@ ElemType pop(linkedstack stack){
 	return e;
 }
 
-//#endif
+#endif
+
+
+#endif
